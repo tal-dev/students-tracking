0cf491eb6ec9fdd03127fccf1803f4986b6f647c
tal-dev <talgat.abykeev@gmail.com>
2021-01-03 01:45:08 -0600

    completed debounce

--- src/components/students/Students.jsx
+++ src/components/students/Students.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react'
-
+import _ from 'lodash'
 import './Students.scss'
 
 export default function Students(props) {
     const { students } = props
+    const [search, setSearch] = useState("")
 
+    function setSearchValue(e) {
+        setSearch(e.target.value)
+    }
+
+    const filtered = students.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
     return (
         <div className="stu-container">
             <div className="students-container">
                 <header>
-                    <input type="text" placeholder="Search by name" autoFocus/>
+                    <input type="text" placeholder="Search by name" autoFocus onChange={_.debounce(setSearchValue, 700)}/>
                 </header>
 
                 <div className="mainSection">
                     {
-                        students.map(item => {
+                        filtered.map(item => {
                             return <div className="single-student" key={item.id}>{item.name}</div>
                         })
                     }
